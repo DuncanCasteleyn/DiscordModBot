5347c890da0ab90f9c03f4ebd2bb0bbbfb595680
Dunciboy <dunciboy@gmail.com>
2017-08-01 14:27:50 +0200

    Added logic to event manager command
    Wrote logic for command to get into event manager sequence.
    Wrote basic logic for event manager sequence.

--- src/main/kotlin/net/dunciboy/discord_bot/EventsManager.kt
+++ src/main/kotlin/net/dunciboy/discord_bot/EventsManager.kt
@@ -2,6 +2,7 @@ package net.dunciboy.discord_bot
 
 import net.dunciboy.discord_bot.commands.CommandModule
 import net.dunciboy.discord_bot.sequence.Sequence
+import net.dv8tion.jda.core.Permission
 import net.dv8tion.jda.core.entities.MessageChannel
 import net.dv8tion.jda.core.entities.User
 import net.dv8tion.jda.core.events.ReadyEvent
@@ -11,17 +12,23 @@ import java.nio.file.Path
 import java.nio.file.Paths
 import java.time.OffsetDateTime
 import java.util.*
+import java.util.concurrent.TimeUnit
+import kotlin.collections.ArrayList
 
 class EventsManager : ListenerAdapter() {
     private lateinit var events: HashMap<Long, ArrayList<Event>>
 
     companion object {
         const private val EVENTS_LIST_DESCRIPTION = "Shows a list with currently planned events"
+        const private val EVENT_MANAGER_DESCRIPTION: String = "Allows you to manage events."
         private val EVENTS_LIST_ALIASES: Array<String> = arrayOf("EventsList")
+        private val EVENT_MANAGER_ALIASES: Array<String> = arrayOf("EventManager", "ManageEvents")
         private val FILE_PATH: Path = Paths.get("Events.json")
     }
 
-    override fun onReady(event: ReadyEvent?) {
+    override fun onReady(event: ReadyEvent) {
+        val guildIds: ArrayList<Long> = ArrayList()
+        event.jda.guilds.map { guildIds.add(it.idLong) }
         TODO("Not implemented. Need to Init HashMap by retrieving all guild ids and then retrieve existing events from a JSON file, then filter expired events if needed and remove empty HashMap values")
     }
 
@@ -38,28 +45,47 @@ class EventsManager : ListenerAdapter() {
         }
     }
 
-    class EventManagerCommand : CommandModule(ALIASES, null, DESCRIPTION) {
-        companion object {
-            private val ALIASES: Array<String> = arrayOf("EventManager", "ManageEvents")
-            const private val DESCRIPTION: String = "Allows you to manage events."
-        }
+    inner class EventManagerCommand : CommandModule(EVENT_MANAGER_ALIASES, null, EVENT_MANAGER_DESCRIPTION) {
 
         override fun commandExec(event: MessageReceivedEvent, command: String, arguments: String?) {
-            TODO("not implemented")
+            if (event.member.hasPermission(Permission.MANAGE_ROLES)) {
+                event.jda.addEventListener(EventMangerSequence(event.author, event.channel))
+            } else {
+                event.channel.sendMessage("You do not have permission to manage events.").queue { it.delete().queueAfter(1, TimeUnit.MINUTES) }
+            }
         }
     }
 
-    inner class EventMangerSequence(user: User, channel: MessageChannel, cleanAfterSequence: Boolean = true, informUser: Boolean = true) : Sequence(user, channel, cleanAfterSequence, informUser) {
-        override fun onMessageReceivedDuringSequence(event: MessageReceivedEvent) {
-            TODO("not implemented")
+    inner class EventMangerSequence(user: User, channel: MessageChannel, cleanAfterSequence: Boolean = true, informUser: Boolean = true, private var sequenceNumber: Int = 0) : Sequence(user, channel, cleanAfterSequence, informUser) {
+        init {
+            channel.sendMessage(user.asMention + " Would you like to add or remove an event? Please answer with \"add\" or \"remove\".")
         }
 
+        override fun onMessageReceivedDuringSequence(event: MessageReceivedEvent) {
+            when (sequenceNumber) {
+                0 -> {
+                    when (event.message.rawContent) {
+                        "add" -> {
+                            sequenceNumber = 1
+                            TODO("Logic to read times not implemented yet.")
+                        }
+                        "remove" -> {
+                            sequenceNumber = 2
+                            TODO("Logic to modify events not implemented yet.")
+                        }
+                        else -> {
+                            super.channel.sendMessage("Wrong answer. Please answer with \"add\" or \"remove\"").queue { super.addMessageToCleaner(it) }
+                        }
+                    }
+                }
+            }
+        }
     }
 
     inner class EventsList : CommandModule(EVENTS_LIST_ALIASES, null, EVENTS_LIST_DESCRIPTION) {
 
         override fun commandExec(event: MessageReceivedEvent, command: String, arguments: String?) {
-            TODO("not implemented")
+            TODO("Printing of events is not implemented yet.")
         }
     }
 }
