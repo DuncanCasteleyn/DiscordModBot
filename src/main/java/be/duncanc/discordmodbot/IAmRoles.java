51ebf23708aa3d2d732c3a8a0c80f8468b901944
Duncan Casteleyn <10881109+DuncanCasteleyn@users.noreply.github.com>
2017-11-13 12:04:34 +0100

    Wrote some logic to modify existing categories

--- src/main/java/be/duncanc/discordmodbot/IAmRoles.java
+++ src/main/java/be/duncanc/discordmodbot/IAmRoles.java
@@ -30,6 +30,7 @@ import be.duncanc.discordmodbot.utils.jsontojavaobject.JSONKey;
 import be.duncanc.discordmodbot.utils.jsontojavaobject.JSONToJavaObject;
 import net.dv8tion.jda.core.MessageBuilder;
 import net.dv8tion.jda.core.entities.MessageChannel;
+import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import org.jetbrains.annotations.NotNull;
@@ -48,6 +49,7 @@ public class IAmRoles extends CommandModule {
     private static final String DESCRIPTION_I_AM_NOT = "Can be used to remove a role from yourself.";
     private static final String[] ALIASES_I_AM = new String[]{"iam"};
     private static final String DESCRIPTION_I_AM = "Can be used to self assign a role.";
+    private final HashMap<Long, ArrayList<IAmRolesCategory>> iAmRoles;
 
     public IAmRoles() {
         super(ALIASES, null, DESCRIPTION);
@@ -64,8 +66,6 @@ public class IAmRoles extends CommandModule {
         });*/
     }
 
-    private HashMap<Long, ArrayList<IAmRolesCategory>> iAmRoles;
-
     @Override
     public void commandExec(@NotNull MessageReceivedEvent event, @NotNull String command, String arguments) {
         event.getJDA().addEventListener(new IAmRolesSequence(event.getAuthor(), event.getChannel()));
@@ -73,13 +73,20 @@ public class IAmRoles extends CommandModule {
 
     public class IAmRolesSequence extends Sequence {
 
-        private byte sequenceNumber;
+        private byte sequenceNumber = 0;
         private String newCategoryName = null;
-        private ArrayList<IAmRolesCategory> iAmRolesCategories = null;
+        private IAmRolesCategory iAmRolesCategory = null;
+        private final ArrayList<IAmRolesCategory> iAmRolesCategories;
 
         IAmRolesSequence(@NotNull User user, @NotNull MessageChannel channel) {
             super(user, channel);
-            sequenceNumber = 0;
+            if (!(channel instanceof TextChannel)) {
+                super.destroy();
+                throw new UnsupportedOperationException("This command must be executed in a guild.");
+            }
+            synchronized (iAmRoles) {
+                iAmRolesCategories = iAmRoles.computeIfAbsent(((TextChannel) channel).getGuild().getIdLong(), k -> new ArrayList<>());
+            }
         }
 
         @Override
@@ -100,7 +107,6 @@ public class IAmRoles extends CommandModule {
                             break;
                         case 1:
                             MessageBuilder deleteCategoryMessage = new MessageBuilder().append("Please select which role category you'd like to delete.");
-                            iAmRolesCategories = iAmRoles.get(event.getGuild().getIdLong());
                             for (int i = 0; i < iAmRolesCategories.size(); i++) {
                                 deleteCategoryMessage.append('\n').append(i).append(". ").append(iAmRolesCategories.get(i).categoryName);
                             }
@@ -108,7 +114,11 @@ public class IAmRoles extends CommandModule {
                             sequenceNumber = 3;
                             break;
                         case 2:
-                            //todo logic to add roles to existing categories or modify if you can only select one role
+                            MessageBuilder modifyCategoryMessage = new MessageBuilder().append("Please select which role category you'd like to modify.");
+                            for (int i = 0; i < iAmRolesCategories.size(); i++) {
+                                modifyCategoryMessage.append('\n').append(i).append(". ").append(iAmRolesCategories.get(i).categoryName);
+                            }
+                            modifyCategoryMessage.buildAll(MessageBuilder.SplitPolicy.NEWLINE).forEach(message -> super.getChannel().sendMessage(message).queue(message1 -> super.addMessageToCleaner(message1)));
                             sequenceNumber = 4;
                             break;
                         default:
@@ -126,57 +136,107 @@ public class IAmRoles extends CommandModule {
                         newCategoryName = event.getMessage().getRawContent();
                         super.getChannel().sendMessage("Please enter if a user can only have one role of this category. true or false?").queue(message -> super.addMessageToCleaner(message));
                     } else {
-                        iAmRoles.get(event.getGuild().getIdLong()).add(new IAmRolesCategory(newCategoryName, Boolean.parseBoolean(event.getMessage().getRawContent())));
+                        synchronized (iAmRolesCategories) {
+                            iAmRolesCategories.add(new IAmRolesCategory(newCategoryName, Boolean.parseBoolean(event.getMessage().getRawContent())));
+                        }
                         super.getChannel().sendMessage("Successfully added new category.").queue(message -> message.delete().queueAfter(1, TimeUnit.MINUTES));
                         super.destroy();
                     }
                     break;
                 case 3:
-                    iAmRolesCategories.remove(Integer.parseInt(event.getMessage().getRawContent()));
+                    synchronized (iAmRolesCategories) {
+                        iAmRolesCategories.remove(Integer.parseInt(event.getMessage().getRawContent()));
+                    }
                     super.getChannel().sendMessage("Successfully removed the category").queue(message -> message.delete().queueAfter(1, TimeUnit.MINUTES));
                     super.destroy();
                     break;
                 case 4:
-                    //todo
+                    iAmRolesCategory = iAmRolesCategories.get(Integer.parseInt(event.getMessage().getRawContent()));
+                    super.getChannel().sendMessage("Please enter the number of the action you'd like to perform.\n" +
+                            "0. Modify the name. Current value: " + iAmRolesCategory.categoryName + "\n" +
+                            "1. Invert if the users can only have one role. Current value" + iAmRolesCategory.canOnlyHaveOne + "\n" +
+                            "2. Add or remove roles.").queue(message -> super.addMessageToCleaner(message));
+                    sequenceNumber = 5;
                     break;
+                case 5:
+                    switch(Byte.parseByte(event.getMessage().getRawContent())) {
+                        case 0:
+                            super.getChannel().sendMessage("Please type a new name for the category.").queue(message -> super.addMessageToCleaner(message));
+                            sequenceNumber = 6;
+                            break;
+                        case 1:
+                        case 2:
+                            //todo
+                    }
+                    break;
+                case 6:
+                    iAmRolesCategory.setCategoryName(event.getMessage().getContent());
+                    super.getChannel().sendMessage("Name successfully changed.").queue(message -> message.delete().queueAfter(1, TimeUnit.MINUTES));
+                    super.destroy();
             }
         }
     }
 
+    /**
+     * A role category
+     *
+     * This class is Thread safe.
+     */
     class IAmRolesCategory {
         private String categoryName;
         private boolean canOnlyHaveOne;
-        private ArrayList<Long> roles;
+        private final ArrayList<Long> roles;
 
-        public IAmRolesCategory(@NotNull String categoryName, boolean canOnlyHaveOne) {
+        /**
+         * Constructor for a new IAmRolesCategory.
+         *
+         * @param categoryName The name of the category.
+         * @param canOnlyHaveOne If only one role can be self assigned from this category or multiple.
+         */
+        IAmRolesCategory(@NotNull String categoryName, boolean canOnlyHaveOne) {
             this.categoryName = categoryName;
             this.canOnlyHaveOne = canOnlyHaveOne;
             this.roles = new ArrayList<>();
         }
 
-        public IAmRolesCategory(@JSONKey(jsonKey = "categoryName") String categoryName, @JSONKey(jsonKey = "canOnlyHaveOne") boolean canOnlyHaveOne, @JSONKey(jsonKey = "roles") JSONArray roles) {
+        /**
+         * A constructor to load an existing IAmRolesCategory from a JSON file.
+         *
+         * @param categoryName The name of the category.
+         * @param canOnlyHaveOne If only one role can be self assigned from this category or multiple.
+         * @param roles
+         */
+        IAmRolesCategory(@JSONKey(jsonKey = "categoryName") String categoryName, @JSONKey(jsonKey = "canOnlyHaveOne") boolean canOnlyHaveOne, @JSONKey(jsonKey = "roles") JSONArray roles) {
             this.categoryName = categoryName;
             this.canOnlyHaveOne = canOnlyHaveOne;
             this.roles = new ArrayList<>(JSONToJavaObject.INSTANCE.toTypedList(roles, Long.class));
         }
 
         @JSONKey(jsonKey = "guildId")
-        public String getCategoryName() {
+        public synchronized String getCategoryName() {
             return categoryName;
         }
 
         @JSONKey(jsonKey = "roles")
-        public List<Long> getRoles() {
+        public synchronized List<Long> getRoles() {
             return Collections.unmodifiableList(roles);
         }
 
         @JSONKey(jsonKey = "canOnlyHaveOne")
-        public boolean canOnlyHaveOne() {
+        public synchronized boolean canOnlyHaveOne() {
             return canOnlyHaveOne;
         }
 
+        synchronized void setCategoryName(String categoryName) {
+            this.categoryName = categoryName;
+        }
+
+        synchronized void setCanOnlyHaveOne(boolean canOnlyHaveOne) {
+            this.canOnlyHaveOne = canOnlyHaveOne;
+        }
+
         @Override
-        public boolean equals(Object o) {
+        public synchronized boolean equals(Object o) {
             if (this == o) {
                 return true;
             }
@@ -190,7 +250,7 @@ public class IAmRoles extends CommandModule {
         }
 
         @Override
-        public int hashCode() {
+        public synchronized int hashCode() {
             return categoryName.hashCode();
         }
     }
