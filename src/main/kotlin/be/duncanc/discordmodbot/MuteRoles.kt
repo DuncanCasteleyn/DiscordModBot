dbb66ef679c4666663902eac28d3e553020ce3a2
Duncan Casteleyn <10881109+DuncanCasteleyn@users.noreply.github.com>
2017-12-28 15:50:45 +0100

    Added message to inform user

--- src/main/kotlin/be/duncanc/discordmodbot/MuteRoles.kt
+++ src/main/kotlin/be/duncanc/discordmodbot/MuteRoles.kt
@@ -12,6 +12,7 @@ import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
 import java.util.*
+import java.util.concurrent.TimeUnit
 
 object MuteRoles : CommandModule(arrayOf("MuteRole"), "Name of the mute role or nothing to remove, the role", "This command allows you to set the mute role for a guild/server") {
 
@@ -19,7 +20,7 @@ object MuteRoles : CommandModule(arrayOf("MuteRole"), "Name of the mute role or 
     private val muteRoles = HashMap<Long, Long>()
 
     init {
-        if(FILE_PATH.toFile().exists()) {
+        if (FILE_PATH.toFile().exists()) {
             val stringBuilder = StringBuilder()
             Files.readAllLines(FILE_PATH).forEach { stringBuilder.append(it) }
             val json = JSONObject(stringBuilder.toString())
@@ -35,9 +36,11 @@ object MuteRoles : CommandModule(arrayOf("MuteRole"), "Name of the mute role or 
         val guildId = event.guild.idLong
         if (arguments == null) {
             muteRoles.remove(guildId)
+            event.channel.sendMessage("Mute role has been removed.").queue { it.delete().queueAfter(1, TimeUnit.MINUTES) }
         } else {
             try {
-                muteRoles.put(guildId, event.guild.getRolesByName(event.message.contentRaw, false)[0].idLong)
+                muteRoles.put(guildId, event.guild.getRolesByName(arguments, false)[0].idLong)
+                event.channel.sendMessage("Role has been set has mute role.").queue { it.delete().queueAfter(1, TimeUnit.MINUTES) }
             } catch (exception: IndexOutOfBoundsException) {
                 throw IllegalArgumentException("Couldn't find any roles with that name.", exception)
             }
@@ -57,12 +60,12 @@ object MuteRoles : CommandModule(arrayOf("MuteRole"), "Name of the mute role or 
         saveMuteRoles()
     }
 
-    fun getMuteRole(guild: Guild) : Role {
+    fun getMuteRole(guild: Guild): Role {
         try {
             val roleId = muteRoles[guild.idLong]!!
 
             return guild.getRoleById(roleId)
-        } catch(npe: NullPointerException) {
+        } catch (npe: NullPointerException) {
             throw IllegalStateException("This guild does not have a mute role set up.", npe)
         }
     }
