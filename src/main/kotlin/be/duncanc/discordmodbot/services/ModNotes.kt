a3f97dffbedc54ae78a91597b77edb2f968491d1
Duncan Casteleyn <10881109+DuncanCasteleyn@users.noreply.github.com>
2018-01-16 18:11:35 +0100

    Added more logic

--- src/main/kotlin/be/duncanc/discordmodbot/services/ModNotes.kt
+++ src/main/kotlin/be/duncanc/discordmodbot/services/ModNotes.kt
@@ -2,23 +2,34 @@ package be.duncanc.discordmodbot.services
 
 import be.duncanc.discordmodbot.commands.CommandModule
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent
+import org.json.JSONObject
+import java.nio.file.Files
 import java.nio.file.Paths
 import java.time.OffsetDateTime
+import java.util.*
+import kotlin.collections.ArrayList
 
 object ModNotes : CommandModule(arrayOf("AddNote"), "[user mention] [note text~]", "This command adds a note to the mentioned user for internal use.") {
     private val FILE_PATH = Paths.get("ModNotes.json")
 
-    private val notes = HashMap<Long, ArrayList<Note>>() //todo think about saving this per user.
+    private val notes = HashMap<Long, ArrayList<Notes>>() //todo think about saving this per user.
 
     enum class NoteType {
         NORMAL, WARN, MUTE, KICK
     }
 
     private fun saveToFile() {
-        TODO()
+        val json = JSONObject(notes)
+        Files.write(FILE_PATH, Collections.singletonList(json.toString()))
     }
 
     private fun loadFromFile() {
+        val fileContent = Files.readAllLines(FILE_PATH)
+        val stringBuilder = StringBuilder()
+        fileContent.forEach {
+            stringBuilder.append(it)
+        }
+        val json = JSONObject(stringBuilder.toString())
         TODO()
     }
 
@@ -30,5 +41,9 @@ object ModNotes : CommandModule(arrayOf("AddNote"), "[user mention] [note text~]
         TODO()
     }
 
-    class Note(val userId: Long,note: String, val type: NoteType, creationDate: OffsetDateTime = OffsetDateTime.now())
+    class Note(note: String, val type: NoteType, creationDate: OffsetDateTime = OffsetDateTime.now())
+
+    class Notes(val userId: Long) {
+        val notes = ArrayList<Note>()
+    }
 }
