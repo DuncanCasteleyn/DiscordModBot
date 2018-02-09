b395894f0e9f763f9e8d7f475679d5495134e680
Duncan Casteleyn <10881109+DuncanCasteleyn@users.noreply.github.com>
2018-02-09 15:51:28 +0100

    Added load and save logic

--- src/main/kotlin/be/duncanc/discordmodbot/services/ModNotes.kt
+++ src/main/kotlin/be/duncanc/discordmodbot/services/ModNotes.kt
@@ -6,26 +6,55 @@ import net.dv8tion.jda.core.Permission
 import net.dv8tion.jda.core.events.ReadyEvent
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent
 import net.dv8tion.jda.core.exceptions.PermissionException
+import org.json.JSONArray
 import org.json.JSONObject
+import org.slf4j.LoggerFactory
 import java.nio.file.Files
 import java.nio.file.Paths
 import java.time.OffsetDateTime
 import java.util.*
 import java.util.concurrent.TimeUnit
-import kotlin.collections.ArrayList
 import kotlin.collections.HashMap
+import kotlin.collections.HashSet
 
 object ModNotes : CommandModule(arrayOf("AddNote"), "[user mention] [note text~]", "This command adds a note to the mentioned user for internal use.", requiredPermissions = *arrayOf(Permission.MANAGE_ROLES)) {
     private val FILE_PATH = Paths.get("ModNotes.json")
+    private val LOG = LoggerFactory.getLogger(this::class.java)
 
-    private val notes = HashMap<Long, HashMap<Long, ArrayList<Note>>>() //First map long are guild ids, second map long are user ids
+    private val notes = HashMap<Long, HashMap<Long, HashSet<Note>>>() //First map long are guild ids, second map long are user ids
 
-    enum class NoteType {
+    public enum class NoteType {
         NORMAL, WARN, MUTE, KICK
     }
 
+    init {
+        try {
+            loadFromFile()
+        } catch (e: Exception) {
+            LOG.warn("Loading saved notes failed", e)
+        }
+    }
+
     private fun saveToFile() {
-        val json = JSONObject(notes)
+        val json = JSONObject()
+        synchronized(notes) {
+            notes.forEach { guildHashMap ->
+                val users = JSONObject()
+                guildHashMap.value.forEach { userHashMap ->
+                    val notes = JSONArray()
+                    userHashMap.value.forEach {
+                        val note = JSONObject()
+                        note.put("note", it.note)
+                        note.put("authorId", it.authorId)
+                        note.put("creationDate", it.creationDate)
+                        note.put("type", it.type.toString())
+                        notes.put(note)
+                    }
+                    users.put(userHashMap.key.toString(), notes)
+                }
+                json.put(guildHashMap.key.toString(), users)
+            }
+        }
         Files.write(FILE_PATH, Collections.singletonList(json.toString()))
     }
 
@@ -36,19 +65,28 @@ object ModNotes : CommandModule(arrayOf("AddNote"), "[user mention] [note text~]
             stringBuilder.append(it)
         }
         val json = JSONObject(stringBuilder.toString())
+        val loadedNotes = HashMap<Long, HashMap<Long, HashSet<Note>>>() //First map long are guild ids, second map long are user ids
         json.toMap().forEach { guildHashMap ->
-            (guildHashMap.value as HashMap<*, *>).forEach { userHashMap ->
-                val notes = ArrayList<Note>()
-                (userHashMap.value as Array<*>).forEach { note ->
-                    TODO(note.toString())
+            val userNotes = HashMap<Long, HashSet<Note>>()
+            (guildHashMap.value as Map<*, *>).forEach { userMap ->
+                val notes = HashSet<Note>()
+                (userMap.value as List<*>).forEach { note ->
+                    note as Map<*, *>
+                    val noteObject = Note(note["note"] as String, NoteType.valueOf(note["type"] as String), note["authorId"] as Long, OffsetDateTime.parse(note["creationDate"] as String))
+                    notes.add(noteObject)
                 }
+                userNotes[(userMap.key as String).toLong()] = notes
             }
+            loadedNotes[(guildHashMap.key as String).toLong()] = userNotes
+        }
+        synchronized(notes) {
+            notes.putAll(loadedNotes)
         }
     }
 
     override fun commandExec(event: MessageReceivedEvent, command: String, arguments: String?) {
         if (arguments == null) {
-            throw IllegalArgumentException("Arguments are required!")
+            throw IllegalArgumentException("Arguments are required")
         }
 
         val noteText: String
@@ -66,37 +104,57 @@ object ModNotes : CommandModule(arrayOf("AddNote"), "[user mention] [note text~]
     }
 
     private fun addNote(note: String, type: NoteType, userId: Long, guildId: Long, authorId: Long, creationDate: OffsetDateTime = OffsetDateTime.now()) {
-        val guildNotes = notes[guildId] ?: notes.put(guildId, HashMap())!!
-        val userNotes = guildNotes[userId] ?: guildNotes.put(userId, ArrayList())!!
-        userNotes.add(Note(note, type, authorId, creationDate))
-        saveToFile()
+        synchronized(notes) {
+            val guildNotes = if (notes[guildId] == null) {
+                val newHashMap = HashMap<Long, HashSet<Note>>()
+                notes[guildId] = newHashMap
+                newHashMap
+            } else {
+                notes[guildId]!!
+            }
+            val userNotes = if (guildNotes[userId] == null) {
+                val newHashSet = HashSet<Note>()
+                guildNotes[userId] = newHashSet
+                newHashSet
+            } else {
+                guildNotes[userId]!!
+            }
+            userNotes.add(Note(note, type, authorId, creationDate))
+            saveToFile()
+        }
     }
 
-    private fun getNotes(userId: Long, guildId: Long): List<Note> {
+    private fun getNotes(userId: Long, guildId: Long): Set<Note> {
         val guildNotes = notes[guildId]
                 ?: throw IllegalStateException("This guild does not have any notes saved on users yet.")
         val userNotes = guildNotes[userId] ?: throw IllegalStateException("This user does not have any notes yet.")
-        return Collections.unmodifiableList(userNotes)
+        return Collections.unmodifiableSet(userNotes)
     }
 
     override fun onReady(event: ReadyEvent) {
         event.jda.addEventListener(ViewNotes)
     }
 
-    class Note(private val note: String, private val type: NoteType, private val authorId: Long, private val creationDate: OffsetDateTime = OffsetDateTime.now()) {
+    class Note(val note: String, val type: NoteType, val authorId: Long, val creationDate: OffsetDateTime = OffsetDateTime.now()) {
         override fun toString(): String {
-            return "$type note\nnote: $note\n\nCreated: $creationDate\nBy: $authorId)"
+            return "${type.toString().toLowerCase().capitalize()} note\n\nnote: $note\nCreated: $creationDate\nBy: $authorId"
         }
     }
 
     object ViewNotes : CommandModule(arrayOf("ViewNotes"), "[user mention]", "This command show all the notes on a user. This will send to you by DM due to privacy reasons.", requiredPermissions = *arrayOf(Permission.MANAGE_ROLES)) {
         override fun commandExec(event: MessageReceivedEvent, command: String, arguments: String?) {
-            val userNotes = getNotes((event.message.mentionedUsers[0].idLong), event.guild.idLong)
-            event.author.openPrivateChannel().queue {
-                val messageBuilder = MessageBuilder()
-                userNotes.forEach { messageBuilder.append(it.toString()).append('\n') }
-                messageBuilder.buildAll(MessageBuilder.SplitPolicy.NEWLINE).forEach { message ->
-                    it.sendMessage(message).queue { it.delete().queueAfter(15, TimeUnit.MINUTES) }
+            synchronized(notes) {
+                try {
+                    val userNotes = getNotes((event.message.mentionedUsers[0].idLong), event.guild.idLong)
+                    event.author.openPrivateChannel().queue {
+                        val messageBuilder = MessageBuilder()
+                        userNotes.forEach { messageBuilder.append(it.toString()).append('\n') }
+                        messageBuilder.buildAll(MessageBuilder.SplitPolicy.NEWLINE).forEach { message ->
+                            it.sendMessage(message).queue { it.delete().queueAfter(15, TimeUnit.MINUTES) }
+                        }
+                    }
+                } catch (ie: IndexOutOfBoundsException) {
+                    throw IllegalArgumentException("You need to mention a user", ie)
                 }
             }
         }
