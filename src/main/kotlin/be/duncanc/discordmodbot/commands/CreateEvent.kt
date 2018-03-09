c60c1c467aa6de3c90bafda86a92432ef58f09cc
Duncan Casteleyn <10881109+DuncanCasteleyn@users.noreply.github.com>
2018-03-09 22:08:06 +0100

    Fixes after first test

--- src/main/kotlin/be/duncanc/discordmodbot/commands/CreateEvent.kt
+++ src/main/kotlin/be/duncanc/discordmodbot/commands/CreateEvent.kt
@@ -33,11 +33,14 @@ import net.dv8tion.jda.core.events.message.MessageReceivedEvent
 import net.dv8tion.jda.core.events.message.guild.GuildMessageDeleteEvent
 import net.dv8tion.jda.core.events.message.guild.react.GuildMessageReactionAddEvent
 import net.dv8tion.jda.core.events.message.guild.react.GuildMessageReactionRemoveEvent
+import net.dv8tion.jda.core.events.role.RoleDeleteEvent
 import org.json.JSONArray
 import org.json.JSONObject
 import java.nio.file.Files
 import java.nio.file.Paths
 import java.util.*
+import java.util.concurrent.CancellationException
+import java.util.concurrent.ExecutionException
 import kotlin.collections.ArrayList
 
 object CreateEvent : CommandModule(arrayOf("CreateEvent"), "<event id/name> <subscribers role> <emote to react to> <event text>", "Creates an event, including role and message to announce the event", requiredPermissions = *arrayOf(Permission.MANAGE_ROLES)) {
@@ -57,7 +60,7 @@ object CreateEvent : CommandModule(arrayOf("CreateEvent"), "<event id/name> <sub
             val serverEvent = events[event.guild]
             if (serverEvent != null && serverEvent.any { it.announceMessage.idLong == event.messageIdLong }) {
                 val reactedEvent = serverEvent.filter { it.announceMessage.idLong == event.messageIdLong }[0]
-                event.guild.controller.addSingleRoleToMember(event.member, reactedEvent.eventRole)
+                event.guild.controller.addSingleRoleToMember(event.member, reactedEvent.eventRole).reason("Voted on event reaction").queue()
             }
         }
     }
@@ -67,7 +70,7 @@ object CreateEvent : CommandModule(arrayOf("CreateEvent"), "<event id/name> <sub
             val serverEvent = events[event.guild]
             if (serverEvent != null && serverEvent.any { it.announceMessage.idLong == event.messageIdLong }) {
                 val reactedEvent = serverEvent.filter { it.announceMessage.idLong == event.messageIdLong }[0]
-                event.guild.controller.removeSingleRoleFromMember(event.member, reactedEvent.eventRole)
+                event.guild.controller.removeSingleRoleFromMember(event.member, reactedEvent.eventRole).reason("Remove vote on event reaction").queue()
             }
         }
     }
@@ -76,7 +79,24 @@ object CreateEvent : CommandModule(arrayOf("CreateEvent"), "<event id/name> <sub
         synchronized(events) {
             val serverEvent = events[event.guild]
             if (serverEvent != null && serverEvent.any { it.announceMessage.idLong == event.messageIdLong }) {
-                serverEvent.removeAll(serverEvent.filter { it.announceMessage.idLong == event.messageIdLong })
+                val toRemove = serverEvent.filter { it.announceMessage.idLong == event.messageIdLong }.toList()
+                toRemove.forEach {
+                    it.eventRole.delete().queue()
+                }
+                serverEvent.removeAll(toRemove)
+            }
+        }
+    }
+
+    override fun onRoleDelete(event: RoleDeleteEvent) {
+        synchronized(events) {
+            val serverEvent = events[event.guild]
+            if (serverEvent != null && serverEvent.any { it.eventRole.idLong == event.role.idLong }) {
+                val toRemove = serverEvent.filter { it.eventRole.idLong == event.role.idLong }.toList()
+                toRemove.forEach {
+                    it.announceMessage.delete().queue()
+                }
+                serverEvent.removeAll(toRemove)
             }
         }
     }
@@ -121,7 +141,10 @@ object CreateEvent : CommandModule(arrayOf("CreateEvent"), "<event id/name> <sub
                     val newArrayList = ArrayList<EventRole>()
                     eventsArray.forEach {
                         it as JSONObject
-                        newArrayList.add(EventRole(it.getString("eventId"), jda.getRoleById(it.getLong("evenRole")), jda.getEmoteById(it.getLong("reactEmote")), jda.getTextChannelById(it.getLong("announceChannel")).getMessageById(it.getLong("announceMessage")).complete()))
+                        try {
+                            newArrayList.add(EventRole(it.getString("eventId"), jda.getRoleById(it.getLong("eventRole")), jda.getEmoteById(it.getLong("reactEmote")), jda.getTextChannelById(it.getLong("announceChannel")).getMessageById(it.getLong("announceMessage")).complete()))
+                        } catch (ignored: Exception) {
+                        }
                     }
                     loadedEvents[guild] = newArrayList
                 }
@@ -140,61 +163,85 @@ object CreateEvent : CommandModule(arrayOf("CreateEvent"), "<event id/name> <sub
             channel.sendMessage("Please enter the event id/name").queue()
         }
 
-        override fun onMessageReceivedDuringSequence(event: MessageReceivedEvent) {
-            when {
-                eventName == null -> {
-                    eventName = event.message.contentStripped
-                    channel.sendMessage("Please mention the role you wanted to be used.").queue()
-                }
-                eventRole == null -> {
-                    eventRole = event.message.contentDisplay
-                    channel.sendMessage("Please post the emote to be used.").queue()
-                }
-                reactEmote == null -> {
-                    reactEmote = event.message.emotes[0]
-                    channel.sendMessage("Please mention the channel were you want the announcement to be made.").queue()
-                }
-                announceChannel == null -> {
-                    announceChannel = event.message.mentionedChannels[0]
-                    channel.sendMessage("Please enter the announcement text.").queue()
-                }
-                else -> {
-                    val guild = event.guild
+        override fun onMessageReceivedDuringSequence(event: MessageReceivedEvent) = when {
+            eventName == null -> {
+                eventName = event.message.contentStripped
+                channel.sendMessage("What should the name of the new event role be?").queue { super.addMessageToCleaner(it) }
+            }
+            eventRole == null -> {
+                eventRole = event.message.contentDisplay
+                channel.sendMessage("Please post the emote to be used.").queue { super.addMessageToCleaner(it) }
+            }
+            reactEmote == null -> {
+                reactEmote = event.message.emotes[0]
+                channel.sendMessage("Please mention the channel were you want the announcement to be made.").queue { super.addMessageToCleaner(it) }
+            }
+            announceChannel == null -> {
+                announceChannel = event.message.mentionedChannels[0]
+                channel.sendMessage("Please enter the announcement text.").queue { super.addMessageToCleaner(it) }
+            }
+            else -> {
+                val guild = event.guild
 
-                    val newRoleFuture = guild.controller.createRole().submit()
-                    val announceFuture = announceChannel!!.sendMessage(event.message.contentRaw).submit()
-                    if (newRoleFuture.isCompletedExceptionally && announceFuture.isCompletedExceptionally) {
-                        val changeRoleName = newRoleFuture.get().manager.setName(eventRole).submit()
-                        val reactFuture = announceFuture.get().addReaction(reactEmote).submit()
-                        if (!(changeRoleName.isCompletedExceptionally && reactFuture.isCompletedExceptionally)) {
-                            changeRoleName.cancel(true)
-                            newRoleFuture.get().delete().queue()
-                            reactFuture.cancel(true)
+                val newRoleFuture = guild.controller.createRole().submit()
+                val announceFuture = announceChannel!!.sendMessage(event.message.contentRaw).submit()
+                try {
+                    val changeRoleName = newRoleFuture.get().manager.setName(eventRole).submit()
+                    val reactFuture = announceFuture.get().addReaction(reactEmote).submit()
+                    try {
+                        changeRoleName.get()
+                        reactFuture.get()
+                    } catch (exception: Exception) {
+                        try {
                             announceFuture.get().delete().queue()
+                        } catch (ignored: InterruptedException) {
+                        } catch (ignored: ExecutionException) {
+                        } catch (ignored: CancellationException) {
                         }
+                        try {
+                            announceFuture.get().delete().queue()
+                        } catch (ignored: InterruptedException) {
+                        } catch (ignored: ExecutionException) {
+                        } catch (ignored: CancellationException) {
+                        }
+                        throw exception
+                    }
+                } catch (exception: Exception) {
+                    if (exception is ExecutionException || exception is InterruptedException || exception is CancellationException) {
+                        if (!newRoleFuture.cancel(false)) {
+                            try {
+                                newRoleFuture.get().delete().queue()
+                            } catch (ignored: InterruptedException) {
+                            } catch (ignored: ExecutionException) {
+                            } catch (ignored: CancellationException) {
+                            }
+                        }
+                        if (!announceFuture.cancel(false) && announceFuture.isDone) {
+                            try {
+                                announceFuture.get().delete().queue()
+                            } catch (ignored: InterruptedException) {
+                            } catch (ignored: ExecutionException) {
+                            } catch (ignored: CancellationException) {
+                            }
+                        }
+                    }
+                    throw exception
+                }
+                val eventRole = EventRole(eventName!!, newRoleFuture.get(), reactEmote!!, announceFuture.get())
+                synchronized(events) {
+                    if (events[event.guild] != null) {
+                        events[event.guild]!!.add(eventRole)
                     } else {
-                        if (!newRoleFuture.cancel(true) && newRoleFuture.isCompletedExceptionally) {
-                            newRoleFuture.get().delete().queue()
-                        }
-                        if (!announceFuture.cancel(true) && announceFuture.isCompletedExceptionally) {
-                            announceFuture.get().delete().queue()
-                        }
+                        val newArrayList = ArrayList<EventRole>()
+                        newArrayList.add(eventRole)
+                        events[event.guild] = newArrayList
                     }
-                    val eventRole = EventRole(eventName!!, newRoleFuture.get(), reactEmote!!, announceFuture.get())
-                    synchronized(events) {
-                        if (events[event.guild] != null) {
-                            events[event.guild]!!.add(eventRole)
-                        } else {
-                            val newArrayList = ArrayList<EventRole>()
-                            newArrayList.add(eventRole)
-                            events[event.guild] = newArrayList
-                        }
-                        save()
-                    }
+                    save()
                 }
             }
         }
     }
+
 
     class EventRole(val eventId: String, val eventRole: Role, val reactEmote: Emote, internal val announceMessage: Message)
 }
