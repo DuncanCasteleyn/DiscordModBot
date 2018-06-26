ba2d5f28cb4454ebec82dcd8d128537e00b6badd
Duncan Casteleyn <10881109+DuncanCasteleyn@users.noreply.github.com>
2018-06-26 20:46:33 +0200

    Correction to the copyright notice.

--- src/main/kotlin/be/duncanc/discordmodbot/data/repositories/MuteRolesRepository.kt
+++ src/main/kotlin/be/duncanc/discordmodbot/data/repositories/MuteRolesRepository.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018.  Duncan Casteleyn
+ * Copyright 2018 Duncan Casteleyn
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
