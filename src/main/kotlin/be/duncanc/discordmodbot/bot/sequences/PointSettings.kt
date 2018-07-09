532db2f45a83dde999eb442f7d54d111949cd662
Duncan Casteleyn <10881109+DuncanCasteleyn@users.noreply.github.com>
2018-07-09 12:08:14 +0200

    Added sequence to modify point settings

--- src/main/kotlin/be/duncanc/discordmodbot/bot/sequences/PointSettings.kt
+++ src/main/kotlin/be/duncanc/discordmodbot/bot/sequences/PointSettings.kt
@@ -17,36 +17,107 @@
 package be.duncanc.discordmodbot.bot.sequences
 
 import be.duncanc.discordmodbot.bot.commands.CommandModule
+import be.duncanc.discordmodbot.data.entities.GuildPointsSettings
+import be.duncanc.discordmodbot.data.repositories.GuildPointsSettingsRepository
+import net.dv8tion.jda.core.MessageBuilder
 import net.dv8tion.jda.core.Permission
 import net.dv8tion.jda.core.entities.MessageChannel
+import net.dv8tion.jda.core.entities.TextChannel
 import net.dv8tion.jda.core.entities.User
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent
+import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
+import java.util.concurrent.TimeUnit
 
-class PointSettings : CommandModule(
+@Component
+class PointSettings(
+        val guildPointsSettingsRepository: GuildPointsSettingsRepository
+) : CommandModule(
         arrayOf("PointSettings"),
         null,
         "This command allows you to modify the settings for the point system.",
         requiredPermissions = *arrayOf(Permission.ADMINISTRATOR)
 ) {
     override fun commandExec(event: MessageReceivedEvent, command: String, arguments: String?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        event.jda.addEventListener(PointSettingsSequence(event.author, event.textChannel))
     }
 
     @Transactional
-    class PointSettingsSequence(
+    inner class PointSettingsSequence(
             user: User,
             channel: MessageChannel
     ) : Sequence(
             user,
             channel
     ) {
+        var sequenceNumber = 0.toByte()
+
         init {
-            channel.sendMessage("What would you like to do?")
+            val guild = (channel as TextChannel).guild
+            @Suppress("LeakingThis")
+            val guildSettings = guildPointsSettingsRepository.findById(guild.idLong).orElse(GuildPointsSettings(guild.idLong))
+
+            val announceChannelId = guildSettings.announceChannelId
+            val announceChannel = if (announceChannelId != null) {
+                guild.getTextChannelById(announceChannelId)?.name ?: "Chanel no longer exists"
+            } else {
+                "Not set"
+            }
+
+            val messageBuilder = MessageBuilder()
+                    .append("What would you like to do?\n")
+                    .append("\n0. Change max points per reason. Current value: ").append(guildSettings.maxPointsPerReason)
+                    .append("\b1. Change the limit before a summary is announced with the users collected points. Current value: ").append(guildSettings.announcePointsSummaryLimit)
+                    .append("\n2. Change the channel to announce the summary in. Current channel: ").append(announceChannel)
+            channel.sendMessage(messageBuilder.build()).queue { super.addMessageToCleaner(it) }
         }
 
         override fun onMessageReceivedDuringSequence(event: MessageReceivedEvent) {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            when (sequenceNumber) {
+                0.toByte() -> {
+                    when (event.message.contentRaw.toByte()) {
+                        0.toByte() -> {
+                            channel.sendMessage("Please enter the new max amount of allowed points per reason.").queue { super.addMessageToCleaner(it) }
+                            sequenceNumber = 1
+                        }
+                        1.toByte() -> {
+                            channel.sendMessage("Please enter the new limit for a summary to be posted.").queue { super.addMessageToCleaner(it) }
+                            sequenceNumber = 2
+                        }
+                        2.toByte() -> {
+                            channel.sendMessage("Please mention the channel where you want announcement to be made. (These summaries use an everyone ping so do not use a public channel)").queue { super.addMessageToCleaner(it) }
+                            sequenceNumber = 3
+                        }
+                        else -> {
+                            throw IllegalArgumentException("You must select a number between 0 and 2")
+                        }
+                    }
+                }
+                1.toByte() -> {
+                    val guild = (channel as TextChannel).guild
+                    val guildSettings = guildPointsSettingsRepository.findById(guild.idLong).orElse(GuildPointsSettings(guild.idLong))
+                    guildSettings.maxPointsPerReason = event.message.contentRaw.toInt()
+                    guildPointsSettingsRepository.save(guildSettings)
+                    channel.sendMessage("Successfully updated settings.").queue { it.delete().queueAfter(1, TimeUnit.MINUTES) }
+                    super.destroy()
+                }
+                2.toByte() -> {
+                    val guild = (channel as TextChannel).guild
+                    val guildSettings = guildPointsSettingsRepository.findById(guild.idLong).orElse(GuildPointsSettings(guild.idLong))
+                    guildSettings.announcePointsSummaryLimit = event.message.contentRaw.toInt()
+                    guildPointsSettingsRepository.save(guildSettings)
+                    channel.sendMessage("Successfully updated settings.").queue { it.delete().queueAfter(1, TimeUnit.MINUTES) }
+                    super.destroy()
+                }
+                3.toByte() -> {
+                    val guild = (channel as TextChannel).guild
+                    val guildSettings = guildPointsSettingsRepository.findById(guild.idLong).orElse(GuildPointsSettings(guild.idLong))
+                    guildSettings.announceChannelId = event.message.mentionedChannels[0].idLong
+                    guildPointsSettingsRepository.save(guildSettings)
+                    channel.sendMessage("Successfully updated settings.").queue { it.delete().queueAfter(1, TimeUnit.MINUTES) }
+                    super.destroy()
+                }
+            }
         }
     }
 }
