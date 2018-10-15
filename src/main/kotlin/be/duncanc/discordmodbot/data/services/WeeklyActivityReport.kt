5059acfcc201e856fdfa434553b3c1957317ee9b
Duncan Casteleyn <10881109+DuncanCasteleyn@users.noreply.github.com>
2018-10-15 16:57:05 +0200

    Added scheduled task

--- src/main/kotlin/be/duncanc/discordmodbot/data/services/WeeklyActivityReport.kt
+++ src/main/kotlin/be/duncanc/discordmodbot/data/services/WeeklyActivityReport.kt
@@ -18,8 +18,14 @@ package be.duncanc.discordmodbot.data.services
 
 import be.duncanc.discordmodbot.bot.commands.CommandModule
 import be.duncanc.discordmodbot.data.repositories.ActivityReportSettingsRepository
+import net.dv8tion.jda.core.JDA
+import net.dv8tion.jda.core.events.ReadyEvent
+import net.dv8tion.jda.core.events.ShutdownEvent
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent
+import org.slf4j.LoggerFactory
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
 
 @Component
 class WeeklyActivityReport(
@@ -27,9 +33,45 @@ class WeeklyActivityReport(
 ) : CommandModule(
         arrayOf("WeeklyActivitySettings"),
         null,
-        "Allows you to configure weekly reports") {
+        "Allows you to configure weekly reports"
+) {
+    companion object {
+        val LOG = LoggerFactory.getLogger(WeeklyActivityReport::class.java)
+    }
+
+    val instances = HashSet<JDA>()
+
+    override fun onReady(event: ReadyEvent) {
+        instances.add(event.jda)
+    }
+
+    override fun onShutdown(event: ShutdownEvent) {
+        instances.remove(event.jda)
+    }
 
     override fun commandExec(event: MessageReceivedEvent, command: String, arguments: String?) {
         TODO("not implemented")
+    }
+
+    @Scheduled(cron = "0 0 * * 1")
+    @Transactional(readOnly = true)
+    fun sendReports() {
+        activityReportSettingsRepository.findAll().forEach { reportSettings ->
+            val guild = reportSettings.guildId?.let { guildId ->
+                instances.stream().filter { jda ->
+                    jda.getGuildById(guildId) != null
+                }.findFirst().orElse(null)
+            }
+            if (guild != null) {
+                val textChannel = reportSettings.reportChannel?.let { guild.getTextChannelById(it) }
+                if(textChannel != null) {
+                    TODO("not implemented")
+                } else {
+                    LOG.warn("The text channel with id ${reportSettings.reportChannel} was not found on the server/guild. Configure another channel.")
+                }
+            } else {
+                LOG.warn("The guild with id ${reportSettings.guildId} was not found, maybe the bot was removed or maybe you shut down the bot responsible for this server.")
+            }
+        }
     }
 }
