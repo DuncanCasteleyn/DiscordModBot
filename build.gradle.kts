170de535122150930cf3b2a6473f6a4ba774e63a
Duncan Casteleyn <10881109+DuncanCasteleyn@users.noreply.github.com>
2020-07-04 13:58:39 +0200

    Upgrade Spring Boot

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 plugins {
     val kotlinVersion = "1.3.72"
 
-    id("org.springframework.boot") version "2.3.0.RELEASE"
+    id("org.springframework.boot") version "2.3.1.RELEASE"
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
