a64b745aa059b8870902565a424afa521138325d
Duncan Casteleyn <10881109+DuncanCasteleyn@users.noreply.github.com>
2020-10-26 19:57:35 +0100

    Added repository annotations

--- src/main/kotlin/be/duncanc/discordmodbot/data/repositories/DiscordMessageRepository.kt
+++ src/main/kotlin/be/duncanc/discordmodbot/data/repositories/DiscordMessageRepository.kt
@@ -2,5 +2,7 @@ package be.duncanc.discordmodbot.data.repositories
 
 import be.duncanc.discordmodbot.data.entities.DiscordMessage
 import org.springframework.data.keyvalue.repository.KeyValueRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface DiscordMessageRepository : KeyValueRepository<DiscordMessage, Long>
--- src/main/kotlin/be/duncanc/discordmodbot/data/repositories/MemberGateQuestionRepository.kt
+++ src/main/kotlin/be/duncanc/discordmodbot/data/repositories/MemberGateQuestionRepository.kt
@@ -2,5 +2,7 @@ package be.duncanc.discordmodbot.data.repositories
 
 import be.duncanc.discordmodbot.data.entities.MemberGateQuestion
 import org.springframework.data.keyvalue.repository.KeyValueRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface MemberGateQuestionRepository : KeyValueRepository<MemberGateQuestion, Long>
