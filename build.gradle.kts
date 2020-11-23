4a0a85159a84242fc9b82b29a3aa7a0dca0a56f1
Duncan Casteleyn <10881109+DuncanCasteleyn@users.noreply.github.com>
2020-11-23 15:12:47 +0100

    Updated Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
-    val kotlinVersion = "1.4.10"
+    val kotlinVersion = "1.4.20"
 
     id("org.springframework.boot") version "2.4.0"
     id("io.spring.dependency-management") version "1.0.10.RELEASE"
