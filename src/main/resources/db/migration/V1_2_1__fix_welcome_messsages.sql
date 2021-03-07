f90fea20bd177e33db8b3ff852e0556f0de0bdd7
Duncan Casteleyn <10881109+DuncanCasteleyn@users.noreply.github.com>
2021-03-07 18:46:59 +0100

    No need to drop primary key

--- src/main/resources/db/migration/V1_2_1__fix_welcome_messsages.sql
+++ src/main/resources/db/migration/V1_2_1__fix_welcome_messsages.sql
@@ -1,6 +1,2 @@
 ALTER TABLE `guild_member_gate_welcome_messages`
-    DROP PRIMARY KEY;
-ALTER TABLE `guild_member_gate_welcome_messages`
     MODIFY COLUMN `message` VARCHAR(2048) NOT NULL;
-ALTER TABLE `guild_member_gate_welcome_messages`
-    ADD PRIMARY KEY (`guild_member_gate_guild_id`, `image_url`, `message`)
