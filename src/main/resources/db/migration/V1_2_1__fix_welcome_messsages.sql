c05837e7c3ba4584ec8658492f0b590fed0993be
Duncan Casteleyn <10881109+DuncanCasteleyn@users.noreply.github.com>
2021-03-14 17:27:54 +0100

    Perform integration test on real database

--- src/main/resources/db/migration/V1_2_1__fix_welcome_messsages.sql
+++ src/main/resources/db/migration/V1_2_1__fix_welcome_messsages.sql
@@ -1,2 +1,8 @@
 ALTER TABLE `guild_member_gate_welcome_messages`
+    ADD UNIQUE (`guild_member_gate_guild_id`, `image_url`, `message`);
+alter table guild_member_gate_questions
+    DROP CONSTRAINT FKhadujm5yqqs7x4f61eibshr5r;
+ALTER TABLE `guild_member_gate_welcome_messages`
+    DROP PRIMARY KEY;
+ALTER TABLE `guild_member_gate_welcome_messages`
     MODIFY COLUMN `message` VARCHAR(2048) NOT NULL;
