3c7197cf37b3d6ecf5ab4face83a7432f0b32a05
Duncan Casteleyn <10881109+DuncanCasteleyn@users.noreply.github.com>
2026-05-16 11:57:54 +0200

    fix: set a read timeout for http request in NarouNovelApiClientConfig

--- src/main/kotlin/be/duncanc/discordmodbot/narou/novel/api/NarouNovelApiClientConfig.kt
+++ src/main/kotlin/be/duncanc/discordmodbot/narou/novel/api/NarouNovelApiClientConfig.kt
@@ -2,10 +2,12 @@ package be.duncanc.discordmodbot.narou.novel.api
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.client.JdkClientHttpRequestFactory
 import org.springframework.web.client.RestClient
 import org.springframework.web.client.support.RestClientAdapter
 import org.springframework.web.service.invoker.HttpServiceProxyFactory
 import org.springframework.web.service.invoker.createClient
+import java.time.Duration
 
 @Configuration
 class NarouNovelApiClientConfig {
@@ -15,8 +17,13 @@ class NarouNovelApiClientConfig {
 
     @Bean
     fun narouNovelApiClient(restClientBuilder: RestClient.Builder): NarouNovelApiClient {
+        val requestFactory = JdkClientHttpRequestFactory()
+
+        requestFactory.setReadTimeout(Duration.ofSeconds(10))
+
         val restClient = restClientBuilder
             .baseUrl(BASE_URL)
+            .requestFactory(requestFactory)
             .build()
 
         val proxyFactory = HttpServiceProxyFactory
